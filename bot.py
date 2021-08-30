b02491ef986a521fbe21aeb24540db6046f8dd96
hamzagamiet <gamiethamza@gmail.com>
2021-08-30 02:24:20 +0100

    initial commit

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import requests
 import asyncio
 
-TOKEN = "ODYyNTMxMjE4NzA4NDk2NDM1.YOZsyw.eseGQs_A2CW9TN2Zew2mCyxjaGY"
+TOKEN = ""
 
 bot = commands.Bot(command_prefix = "!")
 buttons = ButtonsClient(bot)
